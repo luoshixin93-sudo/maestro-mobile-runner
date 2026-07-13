3e442e2c881dea5cb73b74c198f7ab320bbbafd3
QTPhone Cloud <luoshixin93@gmail.com>
2026-07-13 09:34:25 +0800

    Add main.go

---
+++ main.go
@@ -0,0 +1,8 @@
+// Package main is the entry point for maestro-mobile-runner CLI.
+package main
+
+import "github.com/luoshixin93-sudo/maestro-mobile-runner/pkg/cli"
+
+func main() {
+	cli.Execute()
+}
